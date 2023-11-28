3843416c3f067d54615e71dd61e55fb609f5c3c1
SuryanshSrijan <suryanshsb15@gmail.com>
2023-11-28 20:51:36 +0530

    heapphase

--- phase2/market3.cpp
+++ phase2/market3.cpp
@@ -22,15 +22,15 @@ struct order{
 
 struct order_heap {
 
-    bool comp(const order a, const order b) {
-        return a.prc < b.prc;
+    bool comp(const order * a, const order * b) {
+        return a->prc < b->prc;
     }
 
     int sz;
-    std::vector <order> store;
+    std::vector <order*> store;
     order_heap() {sz = 0;}
     bool isEmpty() {return (sz==0);}
-    void insert(order a) {
+    void insert(order* a) {
         store.push_back(a);
         for(int i=sz; i>0; i=(i-1)/2) {
             if(!comp(store[(i-1)/2],a)) break;
@@ -48,49 +48,53 @@ struct order_heap {
             if(comp(store[c],store[r])) c = r;
         }
         if(c == i) return;
-        order temp = store[i];
+        order * temp = store[i];
         store[i] = store[c];
         store[c] = temp;
         heapify(c);
     }
 
-    order max() {
+    order * max() {
         return store[0];
     }
 
     void remove(int i) {
-        store[i].prc = 2147483647;
+        store[i]->prc = 2147483647;
         while(i>0) {
             if(!comp(store[(i-1)/2],store[i])) break;
-            order temp = store[i];
+            order * temp = store[i];
             store[i] = store[(i-1)/2];
             store[(i-1)/2] = temp;
             i = (i-1)/2;
         }
         --sz;
-        order temp = store[sz];
+        order * temp = store[sz];
         store[sz] = store[0];
         store[0] = temp;
         heapify(0);
     }
 
     void remove_expiry(int time) {
-        for(int i=0;i<sz;++i) {
-            if((store[i].ed_time != -1) && (store[i].ed_time<time)) remove(i);
+        for(int i=sz-1;i>=0;--i) {
+            if((store[i]->ed_time != -1) && ((store[i]->ed_time)<time)) remove(i);
         }
     }
 };
 
 struct stock{
-    order_heap buy_orders;
-    order_heap sell_orders;
+    order_heap* buy_orders;
+    order_heap* sell_orders;
+    stock() {
+        buy_orders = new order_heap();
+        sell_orders = new order_heap();
+    }
 };
 
 struct broker {
     std::string name;
 
 };
-std::vector<stock> stock_vector;
+std::vector<stock *> stock_vector;
 market::market(int argc, char** argv)
 {
     std::ifstream file("output.txt");
@@ -122,14 +126,14 @@ void market::start()
 	for(int i=0;i<data.size();++i ) {
         // std::cout<<"HELLO: "<<i<<std::endl;
         int L = data[i].size();
-        order ord;                   //created an order
-        ord.st_time = stoi(data[i][0]);
-        if(stoi(data[i][L-1]) == -1) ord.ed_time = -1;
-        else ord.ed_time = stoi(data[i][L-1]) + ord.st_time;
-        ord.qnt = stoi(data[i][L-2]);
+        order * ord = new order();                   //created an order
+        ord->st_time = stoi(data[i][0]);
+        if(stoi(data[i][L-1]) == -1) ord->ed_time = -1;
+        else ord->ed_time = stoi(data[i][L-1]) + ord->st_time;
+        ord->qnt = stoi(data[i][L-2]);
         // std::string bro = data[i][0];
-        ord.prc = stoi(data[i][L-3]);
-        ord.owner = data[i][1];
+        ord->prc = stoi(data[i][L-3]);
+        ord->owner = data[i][1];
 
         std::string stock_name;         //derived the stock name to put it into map stocks and to keep track of its index in vector stock_vector
 
@@ -141,80 +145,80 @@ void market::start()
         auto it=stocks.find(stock_name); //if not found then a completely new stock so input it into the database
 
         if(it==nullptr){
-            struct stock st;
+            stock * st = new stock();
             if(data[i][2]=="SELL") {
-                ord.prc *= -1;
-                st.sell_orders.insert(ord);
+                ord->prc *= -1;
+                st->sell_orders->insert(ord);
             }
-            else st.buy_orders.insert(ord);
+            else st->buy_orders->insert(ord);
             stock_vector.push_back(st);
             stocks.insert(stock_name,stock_index);
             stock_index++;
         }
         else{ //if found then look for trade
-        std::cout<<stock_name<<" ♥ "<<std::endl;
+        // std::cout<<stock_name<<" ♥ "<<std::endl;
             int index=it->value;
-            struct stock st = stock_vector[index]; //this is the stock of which the order is
+            stock * st = stock_vector[index]; //this is the stock of which the order is
             if(data[i][2]=="SELL"){
-                std::cout<<st.buy_orders.sz<<std::endl;
-                ord.print();
-                // st.buy_orders.remove_expiry(ord.st_time);
-                std::cout<<st.buy_orders.sz<<" "<<stock_name<<std::endl;
-                if(st.buy_orders.isEmpty()) {
-                    ord.prc *= -1;
-                    st.sell_orders.insert(ord);
+                // std::cout<<st->buy_orders->sz<<std::endl;
+                // ord->print();
+                st->buy_orders->remove_expiry(ord->st_time);
+                // std::cout<<st->buy_orders->sz<<" "<<stock_name<<std::endl;
+                if(st->buy_orders->isEmpty()) {
+                    ord->prc *= -1;
+                    st->sell_orders->insert(ord);
                     continue;
                 }
-                order best = st.buy_orders.max();
-                if(ord.qnt <= 0) continue;
-                if(best.prc < ord.prc) {
-                    ord.prc *= -1;
-                    st.sell_orders.insert(ord);
+                order * best = st->buy_orders->max();
+                if(ord->qnt <= 0) continue;
+                if(best->prc < ord->prc) {
+                    ord->prc *= -1;
+                    st->sell_orders->insert(ord);
                     continue;
                 }
-                if(best.qnt > ord.qnt) {
+                if(best->qnt > ord->qnt) {
                     trades++;
-                    shares += ord.qnt;
-                    best.qnt -= ord.qnt;
-                    std::cout<<best.owner<<" purchased "<<ord.qnt<<" share of "<<stock_name<<" from "<<ord.owner<<" for $"<<best.prc<<"/share"<<std::endl;
+                    shares += ord->qnt;
+                    best->qnt -= ord->qnt;
+                    std::cout<<best->owner<<" purchased "<<ord->qnt<<" share of "<<stock_name<<" from "<<ord->owner<<" for $"<<best->prc<<"/share"<<std::endl;
                     continue;
                 }
                 else {
                     trades++;
-                    shares += best.qnt;
-                    st.buy_orders.remove(0);
-                    ord.prc *= -1;
-                    st.sell_orders.insert(ord);
-                    std::cout<<best.owner<<" purchased "<<best.qnt<<" share of "<<stock_name<<" from "<<ord.owner<<" for $"<<best.prc<<"/share"<<std::endl;
+                    shares += best->qnt;
+                    st->buy_orders->remove(0);
+                    ord->prc *= -1;
+                    st->sell_orders->insert(ord);
+                    std::cout<<best->owner<<" purchased "<<best->qnt<<" share of "<<stock_name<<" from "<<ord->owner<<" for $"<<best->prc<<"/share"<<std::endl;
                     continue;
                 }
             }
             else{
-                st.sell_orders.remove_expiry(ord.st_time);
-                if(st.sell_orders.isEmpty()) {
-                    st.buy_orders.insert(ord);
-                if(stock_name == "AMD") std::cout<<st.sell_orders.sz<<"♥♥"<<st.buy_orders.sz<<std::endl;
+                st->sell_orders->remove_expiry(ord->st_time);
+                if(st->sell_orders->isEmpty()) {
+                    st->buy_orders->insert(ord);
+                // if(stock_name == "AMD") std::cout<<st->sell_orders->sz<<"♥♥"<<st->buy_orders->sz<<std::endl;
                     continue;
                 }
-                order best = st.sell_orders.max();
-                if(ord.qnt <= 0) continue;
-                if(best.prc + ord.prc < 0) {
-                    st.buy_orders.insert(ord);
+                order * best = st->sell_orders->max();
+                if(ord->qnt <= 0) continue;
+                if(best->prc + ord->prc < 0) {
+                    st->buy_orders->insert(ord);
                     continue;
                 }
-                if(best.qnt > ord.qnt) {
+                if(best->qnt > ord->qnt) {
                     trades++;
-                    shares += ord.qnt;
-                    best.qnt -= ord.qnt;
-                    std::cout<<ord.owner<<" purchased "<<ord.qnt<<" share of "<<stock_name<<" from "<<best.owner<<" for $"<<(-1)*best.prc<<"/share"<<std::endl;
+                    shares += ord->qnt;
+                    best->qnt -= ord->qnt;
+                    std::cout<<ord->owner<<" purchased "<<ord->qnt<<" share of "<<stock_name<<" from "<<best->owner<<" for $"<<(-1)*best->prc<<"/share"<<std::endl;
                     continue;
                 }
                 else {
                     trades++;
-                    shares += best.qnt;
-                    st.buy_orders.remove(0);
-                    st.buy_orders.insert(ord);
-                    std::cout<<ord.owner<<" purchased "<<best.qnt<<" share of "<<stock_name<<" from "<<best.owner<<" for $"<<(-1)*best.prc<<"/share"<<std::endl;
+                    shares += best->qnt;
+                    st->buy_orders->remove(0);
+                    st->buy_orders->insert(ord);
+                    std::cout<<ord->owner<<" purchased "<<best->qnt<<" share of "<<stock_name<<" from "<<best->owner<<" for $"<<(-1)*best->prc<<"/share"<<std::endl;
                     continue;
                 }
             }
